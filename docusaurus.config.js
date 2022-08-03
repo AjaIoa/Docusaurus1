fb32f67c171692b21b9291dc99d2e9add5ef06d5
AjaIoa <ioan.ajaygosh@snapcom.fr>
2022-08-03 16:35:47 +0200

    jitsi categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/AjaIoa/Docusaurus1/tree/master/',
+            'https://github.com/AjaIoa/Docusaurus1/',
         },
         blog: {
           showReadingTime: true,
