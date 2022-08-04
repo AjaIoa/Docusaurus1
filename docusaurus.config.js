faa64128abd20c10bbef996452f19c2a0e9c8af1
AjaIoa <110535664+AjaIoa@users.noreply.github.com>
2022-08-04 12:23:58 +0200

    modif github2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Docusaurus TEST',
+  title: 'Docusaurus TEST2',
   tagline: 'Documenter c\'est cool',
   url: 'https://AjaIoa.github.io',
   baseUrl: '/Docusaurus1/',
